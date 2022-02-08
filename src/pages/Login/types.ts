d3bef45eef6d937d96aecc05a2dd3d1f6f7b37f8
Vadym Samitashvili <vadym.samitashvili@nixsolutions.com>
2022-02-08 18:27:09 +0200

    tests/log-in-page

--- src/pages/Login/types.ts
+++ src/pages/Login/types.ts
@@ -1,5 +1,6 @@
 import { IUserAuthenticationData } from "../../types";
 
 export interface LoginProps {
+  onSubmit: (values: IUserAuthenticationData) => void;
   userAuthentication: (values: IUserAuthenticationData) => void | string;
 }
