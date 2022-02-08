46b3e9524ceeb57e7f1d9e97a589deb72f2a52f9
V4d1kRed <totalgol2015@gmail.com>
2022-02-08 18:20:10 +0200

    Add tests for sign up page

--- src/pages/Signup/types.ts
+++ src/pages/Signup/types.ts
@@ -1,6 +1,7 @@
 import { IUserRegistrationData } from "../../types";
 
 export interface SignupProps {
+  onSubmit: (values: IUserRegistrationData) => void;
   userRegistration: (
     userRegistrationData: IUserRegistrationData
   ) => void | string;
